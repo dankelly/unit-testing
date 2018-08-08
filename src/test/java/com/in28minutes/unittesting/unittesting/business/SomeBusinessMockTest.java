7ad3a3b094ecbad4919e6c939968880f49c5e3fd
Dan Kelly <dankelly.6@gmail.com>
2018-08-08 10:56:34 -0400

    Step 7 - DRY up mocked tests

--- src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -1,50 +1,40 @@
 package com.in28minutes.unittesting.unittesting.business;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 
 public class SomeBusinessMockTest {
 
+	SomeBusinessImpl business = new SomeBusinessImpl();
+	SomeDataService dataServiceMock = mock(SomeDataService.class);
+
+	@Before
+	public void before() {
+		business.setSomeDataService(dataServiceMock);	
+	}
+	
 	@Test
 	public void calculateSumUsingDataService_basic() {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		
-		SomeDataService dataServiceMock = mock(SomeDataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
-		business.setSomeDataService(dataServiceMock);
-		
-		int actualResult = business.calculateSumUsingDataService();
-		int expectedResult = 6;
-		assertEquals(expectedResult, actualResult);
+		assertEquals(6, business.calculateSumUsingDataService());
 	}
 
 	@Test
 	public void calculateSumUsingDataService_empty() {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		
-		SomeDataService dataServiceMock = mock(SomeDataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
-		business.setSomeDataService(dataServiceMock);
-		
-		int actualResult = business.calculateSumUsingDataService();
-		int expectedResult = 0;
-		assertEquals(expectedResult, actualResult);
+		assertEquals(0, business.calculateSumUsingDataService());
 	}
 
 	@Test
 	public void calculateSumUsingDataService_oneValue() {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		
-		SomeDataService dataServiceMock = mock(SomeDataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {5});
-		business.setSomeDataService(dataServiceMock);
-		
-		int actualResult = business.calculateSumUsingDataService();
-		int expectedResult = 5;
-		assertEquals(expectedResult, actualResult);
+		assertEquals(5, business.calculateSumUsingDataService());
 	}
 
 }
