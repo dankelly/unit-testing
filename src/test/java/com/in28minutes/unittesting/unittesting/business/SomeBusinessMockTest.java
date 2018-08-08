b84cd1249ada04bc700e5da222884d00ff4ed5fd
Dan Kelly <dankelly.6@gmail.com>
2018-08-08 10:40:11 -0400

    Step 6 - Implement first mocked test

---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -0,0 +1,42 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+import com.in28minutes.unittesting.unittesting.data.SomeDataService;
+
+public class SomeBusinessMockTest {
+
+	@Test
+	public void calculateSumUsingDataService_basic() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		
+		SomeDataService dataServiceMock = mock(SomeDataService.class);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
+		business.setSomeDataService(dataServiceMock);
+		
+		int actualResult = business.calculateSumUsingDataService();
+		int expectedResult = 6;
+		assertEquals(expectedResult, actualResult);
+	}
+/*
+	@Test
+	public void calculateSumUsingDataService_empty() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		business.setSomeDataService(new SomeDataServiceStub());
+		int actualResult = business.calculateSumUsingDataService();
+		int expectedResult = 0;
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void calculateSumUsingDataService_oneValue() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		business.setSomeDataService(new SomeDataServiceStub(new int[] {5}));
+		int actualResult = business.calculateSumUsingDataService();
+		int expectedResult = 5;
+		assertEquals(expectedResult, actualResult);
+	}
+*/
+}
