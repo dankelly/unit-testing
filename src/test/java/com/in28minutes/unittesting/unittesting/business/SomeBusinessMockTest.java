40f6493fa9addcaac97ef7922a2352a4edd4e9a1
Dan Kelly <dankelly.6@gmail.com>
2018-08-08 11:02:17 -0400

    Step 8 - Injecting mocks with Mockito annotations

--- src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -1,24 +1,25 @@
 package com.in28minutes.unittesting.unittesting.business;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomeBusinessMockTest {
 
-	SomeBusinessImpl business = new SomeBusinessImpl();
-	SomeDataService dataServiceMock = mock(SomeDataService.class);
-
-	@Before
-	public void before() {
-		business.setSomeDataService(dataServiceMock);	
-	}
+	@InjectMocks
+	SomeBusinessImpl business;
 	
+	@Mock
+	SomeDataService dataServiceMock;
+
 	@Test
 	public void calculateSumUsingDataService_basic() {
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
